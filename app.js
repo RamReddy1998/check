666dd236fb541227a71148e015287b9ec7d3b614
Jangamgari Ram Reddy <109741542+RamReddy1998@users.noreply.github.com>
2026-04-17 22:58:14 +0530

    Initialize Express server in app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+// index.js
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from Cloud Run 🚀");
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
